cb77f95ad806fff1490a1147dc2c38b6a614a122
James3rd <161543654+JimmyMacHack@users.noreply.github.com>
2025-12-27 22:19:08 -0600

    Remove HoustonPage import from App.tsx
    Removed import statement for HoustonPage component.

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { Menu, X, Phone, Mail, MapPin } from 'lucide-react';
 import HomePage from './components/HomePage';
 import ServicesPage from './components/ServicesPage';
 import AIChatWidget from './components/AIChatWidget';
-import HoustonPage from "./HoustonPage";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('home');
